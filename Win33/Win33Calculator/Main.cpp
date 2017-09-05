5bf0ab01e987452d49d8a9addc2e5155aa7e6ad3
Thomas Pranic <tjpranic@gmail.com>
2017-09-05 17:47:17 +1000

    Fixed config + removed calculator app.

--- Win33/Win33Calculator/Main.cpp
+++
@@ -1,5 +0,0 @@
-#include "Calculator.h"
-
-int CALLBACK WinMain( HINSTANCE instance, HINSTANCE previousInstance, PSTR commandLine, int showCommand ) {
-    return CalculatorApplication( ).run( );
-}
